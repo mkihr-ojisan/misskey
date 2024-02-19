db8d2b9d40f23140c5919e291b40dad5346b9528
mkihr-ojisan <mkihr.ojisan@gmail.com>
2024-02-19 03:38:12 +0000

    動きのあるMFMをデフォルトで有効にした

--- packages/frontend/src/store.ts
+++ packages/frontend/src/store.ts
@@ -221,7 +221,7 @@ export const defaultStore = markRaw(new Storage('base', {
 	},
 	animatedMfm: {
 		where: 'device',
-		default: false,
+		default: true,
 	},
 	advancedMfm: {
 		where: 'device',
@@ -431,10 +431,10 @@ export const defaultStore = markRaw(new Storage('base', {
 			sfxVolume: 1,
 		},
 	},
-  hemisphere: {
+	hemisphere: {
 		where: 'device',
 		default: hemisphere as 'N' | 'S',
-  },
+	},
 	enableHorizontalSwipe: {
 		where: 'device',
 		default: true,
